8f8378bdfbf1ffd34ce6aea943eb5be71fcefd11
Alleshanthankumarreddy <shanthan6699@gmail.com>
2026-06-04 16:01:13 +0530

    Update axios.ts

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 
   baseURL:
-    "http://localhost:5000/api",
+    "https://ai-assessment-generator-delta.vercel.app/api",
 
   withCredentials: true,
 
