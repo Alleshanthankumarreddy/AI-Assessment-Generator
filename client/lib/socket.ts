89a613f31ff718fb797cb2f636a6b3e68b89df56
Alleshanthankumarreddy <shanthan6699@gmail.com>
2026-07-13 11:11:06 +0530

    connected frontend wiht deployed backend

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,7 +2,7 @@ import { io }
 from "socket.io-client";
 
 const socket = io(
-  "http://localhost:5000",
+  "https://ai-assessment-generator-1.onrender.com",
   {
     withCredentials: true,
     autoConnect: true,
